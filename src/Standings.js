e3ae2a77151f7dd2376a3dd5c05a1c5c6eb86592
Teddy Karambelas <tvkarambelas@gmail.com>
2019-08-11 19:09:40 -0400

    removes unused styles and adds Standings trophies

--- src/Standings.js
+++ src/Standings.js
@@ -5,7 +5,8 @@ const Standings = ({teams}) => (
     <h2>Final Standings</h2>
     <ol className="standings">
       {teams.map(team => (
-        <li key={team.id}>
+        <li data-place={team.place} key={team.id}>
+          <span className="fas fa-trophy"></span>
           <span className="label">{team.place}<sup>{team.placeSuffix}</sup></span> {team.name} ({team.wins}-{team.losses})
         </li>
       ))}
